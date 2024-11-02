caf405b0a6cca283b47d43f2834324c5d7c4ee49
kotipalliGanesh <ganeshkotipalli7665@gmail.com>
2024-11-02 15:00:53 +0530

    Update Comp3.jsx

--- first-app/src/components/Comp3.jsx
+++ first-app/src/components/Comp3.jsx
@@ -3,7 +3,7 @@ import Comp2 from "./Comp2";
 const Comp3=()=>{
     return(
         <>
-            <h1>hello</h1>
+        <h1>hello</h1>
         <h1>using Git we are modified</h1>
         <h2>Iam Ganesh</h2>
         <Comp1></Comp1>
