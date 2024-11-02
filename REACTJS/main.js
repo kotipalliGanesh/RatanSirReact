024606940b0b9090b3083359abe54048c3352e2e
Kotipalli Ganesh <ganeshkotipalli7665@gmail.com>
2024-11-02 13:49:04 +0530

    this is first commit

---
+++ REACTJS/main.js
@@ -0,0 +1,5 @@
+import add, { div, mul, sub } from "./mathop.js"
+console.log(add(10,20))
+console.log(mul(10,20))
+console.log(div(30,5))
+console.log(sub(78,29))
---
+++ REACTJS/mathop.js
@@ -0,0 +1,17 @@
+function add(num1,num2)
+{
+    return num1+num2;
+}
+export default add;
+export function  mul(num1,num2)
+{
+    return num1*num2;
+}
+export function div(num1,num2)
+{
+    return num1/num2;
+} 
+export function sub(num1,num2)
+{
+    return num1-num2;
+}
